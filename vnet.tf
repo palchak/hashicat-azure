ed7a323cf76286b6024eb5824c4f13cb8f1749bb
Chakraborty <pallabcd@hotmail.com>
2021-05-19 23:26:04 -0300

    removing NSG and PIP

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Pallab-Training/network/azurerm"
   version = "3.3.0"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = data.azurerm_resource_group.network.name
 }
